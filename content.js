28147fee97a20aef97534f19fa0dd3318b2ada52
sohantamang <tamangsohan@gmail.com>
2019-09-12 13:59:28 -0700

    Updated script to use strict vesrion. Added extension description, cleanup

--- content.js
+++ content.js
@@ -1,66 +1,47 @@
+'use strict';
+
 console.log("Job Application Tracker running");
-
-/*var iframes = document.querySelectorAll('[id^=adsIfrme]')
-for(elt of iframes){
-  console.log(elt);
-  elt.parentNode.removeChild(elt);
-
-}*/
-//$("input[name^='adsIfrme']").remove();
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse){
-  var title = '';
-  var company = '';
-  var location = '';
-  var job_description = '';
-  var current_url = window.location.toString();
-  console.log(current_url);
-  var jobsite = '';
-
-  if(current_url.startsWith('https://www.indeed.com/jobs?')){
-    jobsite = 'indeed';
-    title = document.getElementById("vjs-jobtitle").innerText;
-    company = document.getElementById("vjs-cn").innerText;
-    location = document.getElementById("vjs-loc").innerText;
-    document.querySelectorAll('#vjs-desc > *').forEach((child) => {
-      job_description += child.innerText;
-      job_description += '\n\n';
-    });
-  }
-
-  else if(current_url.startsWith('https://www.glassdoor.com/Job/')){
-    console.log("glassdoor");
-    jobsite = 'glassdoor';
-    company = document.querySelector('.employerName').innerText;
-    title = document.querySelector('.header h1').innerText;
-    location = document.querySelector('.compInfo').lastElementChild.innerText;
-    document.querySelectorAll('#JobDescriptionContainer > *').forEach((child) => {
-      job_description += child.innerText;
-      job_description += '\n\n';
-    });
-  }
-
-  var response = {
-    jobsite: jobsite,
-    URL: current_url,
-    title: title,
-    company: company,
-    location: location,
-    description: job_description,
+  var job_info = {
+    jobsite: '',
+    URL: '',
+    title: '',
+    company: '',
+    location: '',
+    description: ''
   };
+
+  job_info.URL = window.location.toString();
+  console.log(message);
+  console.log(job_info.URL);
+
+  if(job_info.URL.startsWith('https://www.indeed.com/jobs?')){
+    job_info.jobsite = 'indeed';
+    job_info.title = document.getElementById("vjs-jobtitle").innerText;
+    job_info.company = document.getElementById("vjs-cn").innerText;
+    job_info.location = document.getElementById("vjs-loc").innerText;
+    document.querySelectorAll('#vjs-desc > *').forEach((child) => {
+      job_info.description += child.innerText;
+      job_info.description += '\n\n';
+    });
+  }
+
+  else if(job_info.URL.startsWith('https://www.glassdoor.com/Job/')){
+    console.log("glassdoor");
+    job_info.jobsite = 'glassdoor';
+    job_info.company = document.querySelector('.employerName').innerText;
+    job_info.title = document.querySelector('.header h1').innerText;
+    job_info.location = document.querySelector('.compInfo').lastElementChild.innerText;
+    document.querySelectorAll('#JobDescriptionContainer > *').forEach((child) => {
+      job_info.description += child.innerText;
+      job_info.description += '\n\n';
+    });
+  }
 
   console.log(response);
   if(job_description !== '')
     chrome.runtime.sendMessage(response);
 }
-/*
-  job_desc:
-  title:
-  company:
-  location:
-  date:
-
-  filename: timestamp-position-company.txt
-  */
